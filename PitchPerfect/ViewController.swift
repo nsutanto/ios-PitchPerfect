33143fafdd1abc51c9210645c87d839038e29d6f
Nico <nicholas.sutanto@gmail.com>
2017-07-23 16:03:18 -0400

    Adding Asset Images

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -11,14 +11,17 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        stopRecordingButton.isEnabled = false
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -27,11 +30,15 @@ class ViewController: UIViewController {
     }
 
     @IBAction func recordAudio(_ sender: Any) {
+        recordButton.isEnabled = false
+        stopRecordingButton.isEnabled = true
         recordingLabel.text = "Recording in Progress"
     }
 
     @IBAction func stopRecording(_ sender: Any) {
-        
+        recordButton.isEnabled = true
+        stopRecordingButton.isEnabled = false
+        recordingLabel.text = "Tab to Record"
     }
 }
 
